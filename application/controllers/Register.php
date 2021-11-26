466a48534bdbb10efe8dbfbca07100989d0f02d6
hdpolover <hendrapolover@gmail.com>
2021-11-26 23:15:19 +0700

    up

---
+++ application/controllers/Register.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Register extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('register');
+    }
+
+    public function login() {
+        $this->load->view('login');
+    }
+}
